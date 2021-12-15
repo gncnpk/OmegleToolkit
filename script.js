10f66f84206bd0b317eb1a1e235730172592178d
Damian J Pound <damian@thebestisp.com>
2021-12-14 22:54:37 -0600

    Add semicolons

--- script.js
+++ script.js
@@ -11,9 +11,9 @@
 
 (function () {
     'use strict';
-    var blacklist = ['phrase1', 'phrase2', 'phrase3', 'phrase4', 'phrase5', 'phrase6']
-    var enable = '✔️ Enable Script'
-    var disable = '❌ Disable Script'
+    var blacklist = ['phrase1', 'phrase2', 'phrase3', 'phrase4', 'phrase5', 'phrase6'];
+    var enable = '✔️ Enable Script';
+    var disable = '❌ Disable Script';
     function modifySocialButtons() {
         var e = document.getElementById('sharebuttons').children[0];
         var d = document.createElement('button');
@@ -23,24 +23,24 @@
         var d1 = document.createElement('button');
         d1.innerHTML = e1.innerHTML;
         e1.parentNode.replaceChild(d1, e1);
-        document.getElementById('sharebuttons').children[0].href = ''
-        document.getElementById('sharebuttons').children[1].href = ''
-        document.getElementById('sharebuttons').children[0].innerText = disable
-        document.getElementById('sharebuttons').children[1].innerText = enable
+        document.getElementById('sharebuttons').children[0].href = '';
+        document.getElementById('sharebuttons').children[1].href = '';
+        document.getElementById('sharebuttons').children[0].innerText = disable;
+        document.getElementById('sharebuttons').children[1].innerText = enable;
         document.getElementById('sharebuttons').children[0].onclick = function () { window.stopped = true; console.log('Disabled script!'); };
         document.getElementById('sharebuttons').children[1].onclick = function () { window.stopped = false; console.log('Enabled script!'); };
     }
     function skip() {
         for (let i = 0; i < 3; i++) {
-            document.getElementsByClassName('disconnectbtn')[0].click()
+            document.getElementsByClassName('disconnectbtn')[0].click();
         }
     }
     function verify(element) {
-        var msg = element.children[1].innerText
-        console.log('Checking: ' + msg)
+        var msg = element.children[1].innerText;
+        console.log('Checking: ' + msg);
         if (blacklist.indexOf(msg.toLowerCase()) >= 0) {
-            console.log('Blacklisted phrase detected! Skipping!')
-            skip()
+            console.log('Blacklisted phrase detected! Skipping!');
+            skip();
             window.stopped = false;
         }
     }
@@ -49,8 +49,8 @@
             return false;
         }
         else {
-            var arr = Array.from(document.getElementsByClassName('strangermsg'))
-            arr.forEach(element => verify(element))
+            var arr = Array.from(document.getElementsByClassName('strangermsg'));
+            arr.forEach(element => verify(element));
         }
     }
     window.myInterval = setInterval(check, 1000);
