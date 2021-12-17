a1fcb6ce033ab932b0d83d7bf0f23b10b04280fb
Smooklu <gavin952@hotmail.com>
2021-12-17 17:16:15 -0600

    Added simple geo location

--- script.js
+++ script.js
@@ -11,9 +11,51 @@
 
 (function () {
     'use strict';
-    var blacklist = ['phrase', 'phrase2'];
-    var enable = 'Enable Script';
-    var disable = 'Disable Script';
+    let apiKey = "api-key-here";
+    var blacklist
+    function getBlacklist() {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://raw.githubusercontent.com/Smooklu/OmegleToolkit/main/blacklist.json');
+        xhr.send();
+        xhr.onload = function () {
+            blacklist = JSON.parse(xhr.response);
+        };
+    }
+    getBlacklist()
+    window.oRTCPeerConnection =
+        window.oRTCPeerConnection || window.RTCPeerConnection;
+
+    window.RTCPeerConnection = function (...args) {
+        const pc = new window.oRTCPeerConnection(...args);
+
+        pc.oaddIceCandidate = pc.addIceCandidate;
+
+        pc.addIceCandidate = function (iceCandidate, ...rest) {
+            const fields = iceCandidate.candidate.split(" ");
+
+            console.log(iceCandidate.candidate);
+            const ip = fields[4];
+            if (fields[7] === "srflx") {
+                getLocation(ip);
+            }
+            return pc.oaddIceCandidate(iceCandidate, ...rest);
+        };
+        return pc;
+    };
+
+    let getLocation = async (ip) => {
+        let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+        await fetch(url).then((response) =>
+            response.json().then((json) => {
+                const output = `<img src=${json.country_flag}></img><h2 style="color: orange; display: inline-block;">${json.country_name}</h2>`;
+                document.getElementsByClassName('logitem')[0].innerHTML = output
+            })
+        );
+    };
+    var enable = 'Enable Blacklist'
+    var disable = 'Disable Blacklist'
+    let skiptts = new SpeechSynthesisUtterance('Skipping!');
     function modifySocialButtons() {
         var e = document.getElementById('sharebuttons').children[0];
         var d = document.createElement('button');
@@ -23,42 +65,49 @@
         var d1 = document.createElement('button');
         d1.innerHTML = e1.innerHTML;
         e1.parentNode.replaceChild(d1, e1);
-        document.getElementById('sharebuttons').children[0].href = '';
-        document.getElementById('sharebuttons').children[1].href = '';
-        document.getElementById('sharebuttons').children[0].innerText = disable;
-        document.getElementById('sharebuttons').children[1].innerText = enable;
-        document.getElementById('sharebuttons').children[0].style = 'border-radius: 5px; background-color: red; color: white;';
-        document.getElementById('sharebuttons').children[1].style = 'border-radius: 5px; background-color: green; color: white;';
-        document.getElementById('sharebuttons').children[0].onclick = function () { window.stopped = true; console.log('Disabled script!'); };
-        document.getElementById('sharebuttons').children[1].onclick = function () { window.stopped = false; console.log('Enabled script!'); };
-        document.getElementById('sharebuttons').children[2].remove();
+        document.getElementById('sharebuttons').children[0].href = ''
+        document.getElementById('sharebuttons').children[1].href = ''
+        document.getElementById('sharebuttons').children[0].innerText = disable
+        document.getElementById('sharebuttons').children[1].innerText = enable
+        document.getElementById('sharebuttons').children[0].style = 'border-radius: 5px; background-color: red; color: white;'
+        document.getElementById('sharebuttons').children[1].style = 'border-radius: 5px; background-color: green; color: white;'
+        document.getElementById('sharebuttons').children[0].onclick = function () { window.blackliststopped = true; console.log('Disabled blacklist!'); };
+        document.getElementById('sharebuttons').children[1].onclick = function () { window.blackliststopped = false; console.log('Enabled blacklist!'); };
+        document.getElementById('sharebuttons').children[2].remove()
     }
     function skip() {
         for (let i = 0; i < 3; i++) {
-            document.getElementsByClassName('disconnectbtn')[0].click();
+            document.getElementsByClassName('disconnectbtn')[0].click()
         }
     }
     function verify(element) {
-        var msg = element.children[1].innerText;
-        if (blacklist.indexOf(msg.toLowerCase()) >= 0) {
-            console.log('Blacklisted phrase detected! Skipping! Phrase: ' + msg);
-            skip();
-            window.stopped = false;
+        var msg = element.children[1].innerText
+        if (blacklist.exact.indexOf(msg.toLowerCase()) >= 0) {
+            console.log('Exact match blacklist phrase detected! Skipping! Phrase: ' + msg.toLowerCase())
+            //window.speechSynthesis.speak(skiptts);
+            skip()
+        }
+        else if (blacklist.startswith.some(element => msg.toLowerCase().startsWith(element))) {
+            console.log('Starts with blacklist phrase detected! Skipping! Phrase: ' + msg.toLowerCase())
+            //window.speechSynthesis.speak(skiptts);
+            skip()
         }
     }
     function check() {
-        if (window.stopped) {
+        if (window.blackliststopped) {
             return false;
         }
-        var arr = Array.from(document.getElementsByClassName('strangermsg'))
-        if (arr.length == 0) {
-            return false;
+        else {
+            var arr = Array.from(document.getElementsByClassName('strangermsg'))
+            if (arr.length == 0) {
+                return false;
+            }
+            else {
+                arr.forEach(element => verify(element))
+                console.log('Checking: ' + arr.length + ' messages')
+            }
         }
-        arr.forEach(element => verify(element));
-        console.log('Checking: ' + arr.length + ' messages');
     }
     window.myInterval = setInterval(check, 1000);
     modifySocialButtons();
-    console.log('Current blacklist: ');
-    blacklist.forEach(element => console.log(element));
 })();
