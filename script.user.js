a65b33076e38e2dfcb74a18182eb543f3a3408df
Damian J Pound <damian@thebestisp.com>
2022-01-01 15:54:10 -0600

    Ask Typescript to check the file

--- script.user.js
+++ script.user.js
@@ -8,6 +8,7 @@
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
+// @ts-check
 
 (async function () {
     'use strict';
